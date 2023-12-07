24fd9129c6691764faff23598f8f414014387d45
Jan Cichy <04cichy@gmail.com>
2023-12-07 14:14:34 +0100

    corrected misspells

--- day_03/day_03_answer.py
+++ day_03/day_03_answer.py
@@ -1,6 +1,6 @@
 from day_03_code import NeighboursNums
 
-# In day_02_input.txt put your unique input
+# In day_03.txt put your unique input
 path = 'day_03_input.txt'
 
 answer = NeighboursNums(path).start()
