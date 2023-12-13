b8310297d4e411122afaa04fd97e8f34459bc12a
Jan Cichy <04cichy@gmail.com>
2023-12-14 00:04:02 +0100

    solved day_6_part2

---
+++ day_06_part2/day_06_answer_part2.py
@@ -0,0 +1,11 @@
+from day_06_code_part2 import Race
+
+# In day_06_input_part2.txt put your unique input
+input = 'day_06_input_part2.txt'
+
+# In day_06_output_part2.json will be stored JSON output
+output = 'day_06_output_part2.json'
+
+answer = Race(input, output).start()
+
+print(answer)
---
+++ day_06_part2/day_06_code_part2.py
@@ -0,0 +1,62 @@
+import json
+import numpy as np
+from math import floor, ceil
+
+
+class Race:
+    def __init__(self, input, output):
+        self.data_file = output
+        self.input = open(input, 'r')
+        self.data = {}
+        self.answer = []
+
+    def start(self):
+        self.iterate()
+        self.save()
+        self.solve()
+        return self.result()
+
+    def save(self):
+        with open(self.data_file, 'w') as file:
+            json.dump(self.data, file, indent=4)
+
+    def iterate(self):
+        data = self.data
+
+        data[1] = {}
+        for line in self.input:
+            sep = line.strip().split()
+
+            key = sep[0][:-1]
+            val = int(''.join(sep[1:]))
+
+            data[1].update({key: val})
+
+    def solve(self):
+        data = self.data
+
+        for race in list(data.values()):
+            time = race['Time']
+            distance = race['Distance']
+            coefficients = [-1, time, -distance]
+            a, b = np.roots(coefficients)
+
+            if a == floor(a):
+                a -= 1
+            else:
+                a = floor(a)
+
+            if b == ceil(b):
+                b += 1
+            else:
+                b = ceil(b)
+
+            self.answer.append(a - b + 1)
+
+    def result(self):
+        answer = 1
+
+        for num in self.answer:
+            answer *= num
+
+        return int(answer)
