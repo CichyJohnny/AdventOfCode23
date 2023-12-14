16166f11e88f82d65a6d6ec3156e9f9d0ce3d766
Jan Cichy <04cichy@gmail.com>
2023-12-14 02:15:39 +0100

    almost solved day_07_part2

--- day_07_part2/day_07_code_part2.py
+++ day_07_part2/day_07_code_part2.py
@@ -6,7 +6,7 @@ class Race:
     def __init__(self, input, output):
         self.data_file = output
         self.input = open(input, 'r')
-        self.order = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+        self.order = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
         self.data = {}
         self.answer = 0
 
@@ -38,27 +38,57 @@ class Race:
 
         for num in range(len(data)):
             cards = data[num + 1]['cards']
+            keys = list(Counter(cards).keys())
+            if 'J' in keys:
 
-            if max(Counter(cards).values()) == 5:
-                data[num + 1]['order'] = 6
+                for exchange in keys:
+                    new_cards = [exchange if x == 'J' else x for x in cards]
+                    val = Counter(new_cards).values()
 
-            elif max(Counter(cards).values()) == 4:
-                data[num + 1]['order'] = 5
+                    if max(val) == 5:
+                        data[num + 1]['order'] = max(6, data[num + 1]['order'])
 
-            elif max(Counter(cards).values()) == 3 and min(Counter(cards).values()) == 2:
-                data[num + 1]['order'] = 4
+                    elif max(val) == 4:
+                        data[num + 1]['order'] = max(5, data[num + 1]['order'])
 
-            elif max(Counter(cards).values()) == 3:
-                data[num + 1]['order'] = 3
+                    elif max(val) == 3 and min(val) == 2:
+                        data[num + 1]['order'] = max(4, data[num + 1]['order'])
 
-            elif max(Counter(cards).values()) == 2 and list(Counter(cards).values()).count(2) == 2:
-                data[num + 1]['order'] = 2
+                    elif max(val) == 3:
+                        data[num + 1]['order'] = max(3, data[num + 1]['order'])
 
-            elif max(Counter(cards).values()) == 2:
-                data[num + 1]['order'] = 1
+                    elif max(val) == 2 and list(val).count(2) == 2:
+                        data[num + 1]['order'] = max(2, data[num + 1]['order'])
+
+                    elif max(val) == 2:
+                        data[num + 1]['order'] = max(1, data[num + 1]['order'])
+
+                    else:
+                        data[num + 1]['order'] = 0
 
             else:
-                data[num + 1]['order'] = 0
+                val = Counter(cards).values()
+
+                if max(val) == 5:
+                    data[num + 1]['order'] = 6
+
+                elif max(val) == 4:
+                    data[num + 1]['order'] = 5
+
+                elif max(val) == 3 and min(val) == 2:
+                    data[num + 1]['order'] = 4
+
+                elif max(val) == 3:
+                    data[num + 1]['order'] = 3
+
+                elif max(val) == 2 and list(val).count(2) == 2:
+                    data[num + 1]['order'] = 2
+
+                elif max(val) == 2:
+                    data[num + 1]['order'] = 1
+
+                else:
+                    data[num + 1]['order'] = 0
 
             mapa = []
             for char in cards:
