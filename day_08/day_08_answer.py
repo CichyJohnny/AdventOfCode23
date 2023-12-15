54f48bbaf02734cd53b036bb49a0ddbfa5edd02d
Jan Cichy <04cichy@gmail.com>
2023-12-15 18:41:15 +0100

    resolved day_08

---
+++ day_08/day_08_answer.py
@@ -0,0 +1,11 @@
+from day_08_code import Race
+
+# In day_08_input.txt put your unique input
+input = 'day_08_input.txt'
+
+# In day_08_output.json will be stored JSON output
+output = 'day_08_output.json'
+
+answer = Race(input, output).start()
+
+print(answer)
