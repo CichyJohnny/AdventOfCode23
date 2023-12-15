2dc1a527a4690905e710ac5d6d7f3ef3990433a4
Jan Cichy <04cichy@gmail.com>
2023-12-15 22:02:52 +0100

    resolved day_09_part2

---
+++ day_09_part2/day_09_answer_part2.py
@@ -0,0 +1,11 @@
+from day_09_code_part2 import Prediction
+
+# In day_09_input_part2.txt put your unique input
+input = 'day_09_input_part2.txt'
+
+# In day_09_output_part2.json will be stored JSON output
+output = 'day_09_output_part2.json'
+
+answer = Prediction(input, output).start()
+
+print(answer)
