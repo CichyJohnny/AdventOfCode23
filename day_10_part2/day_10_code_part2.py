068a7fc3b72b2bbb32a4f1ffb2e571751546e6d8
Jan Cichy <04cichy@gmail.com>
2023-12-23 17:13:32 +0100

    resolved day_10_part2

--- day_10_part2/day_10_code_part2.py
+++ day_10_part2/day_10_code_part2.py
@@ -7,10 +7,14 @@ class Maze:
         self.input = open(input, 'r')
         self.data = []
         self.max = 0
+        self.path = []
+        self.answer = 0
 
     def start(self):
         self.iterate()
         self.find_entry()
+        self.clear_trash_pipes()
+        self.count_enclosed_tiles(self.path)
         return self.result()
 
     def iterate(self):
@@ -29,6 +33,7 @@ class Maze:
             for j in range(width):
 
                 if data[i][j] == 'S':
+                    self.path.append((i, j))
                     self.first_map_loop(i, j)
                     break
 
@@ -79,7 +84,8 @@ class Maze:
         index += 1
         self.max = max(index, self.max)
 
-        data[i][j] = str(index)
+        data[i][j] = index
+        self.path.append((i, j))
 
         if last_symbol == '| up' or last_symbol == 'J up' or last_symbol == 'L up':
             if data[i - 1][j] == '|':
@@ -121,7 +127,36 @@ class Maze:
             elif data[i][j + 1] == '7':
                 sml(i, j + 1, '7 down', index=index)
 
+    def clear_trash_pipes(self):
+        data = self.data
+        height = len(data)
+        width = len(data[0])
+
+        for row in range(height):
+
+            for col in range(width):
+
+                try:
+                    if data[row][col] in '|-FL7FJ':
+                        data[row][col] = '.'
+
+                except TypeError:
+                    continue
+
+    def count_enclosed_tiles(self, loop):
+        area = 0
+        n = len(loop)
+
+        for i in range(n):
+            j = (i + 1) % n  # Next tuple inside loop
+
+            area += loop[i][0] * loop[j][1]
+            area -= loop[j][0] * loop[i][1]
+
+        area = abs(area) // 2
+        self.answer = area - n // 2 + 1
+
     def result(self):
-        answer = self.max // 2 + 1
+        answer = self.answer
 
         return answer
