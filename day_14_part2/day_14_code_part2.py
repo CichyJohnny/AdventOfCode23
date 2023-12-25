cd24cbba0490639d10554973ec3371a7128f3a5a
Jan Cichy <04cichy@gmail.com>
2023-12-25 23:21:04 +0100

    resolved day_14_part2

--- day_14_part2/day_14_code_part2.py
+++ day_14_part2/day_14_code_part2.py
@@ -1,8 +1,12 @@
+from copy import deepcopy
+
+
 class Platform:
     def __init__(self, input):
         self.input = open(input, 'r')
         self.data = []
         self.answer = 0
+        self.memory = []
 
     def start(self):
         self.iterate()
@@ -19,25 +23,75 @@ class Platform:
             data.append(line)
 
     def tilt(self):
+        times = 10**9
+        x = 1
+        y = 0
+
+        for i in range(times):
+            self.cycle()
+
+            if self.data not in self.memory:
+                app = deepcopy(self.data)
+                self.memory.append(app)
+
+            else:
+                x = i
+                y = self.memory.index(self.data)
+
+                break
+
+        for i in range((times - y - 1) % (x - y)):
+            self.cycle()
+
+    def cycle(self):
         data = self.data
         height = len(data)
         width = len(data[0])
 
         for row in range(height):
-
             for col in range(width):
-
                 if data[row][col] == 'O':
-                    self.roll(row, col)
+                    self.roll(row, col, 'North')  #
 
-    def roll(self, row, col):
+        for row in range(height):
+            for col in range(width):
+                if data[row][col] == 'O':
+                    self.roll(row, col, 'West')
+
+        for row in reversed(range(height)):
+            for col in reversed(range(width)):
+                if data[row][col] == 'O':
+                    self.roll(row, col, 'South')
+
+        for row in reversed(range(height)):
+            for col in reversed(range(width)):
+                if data[row][col] == 'O':
+                    self.roll(row, col, 'East')
+
+    def roll(self, row, col, dir):
         data = self.data
+
         data[row][col] = 'O'
 
-        if row > 0 and not data[row - 1][col] in ['#', 'O']:
+        if dir == 'North' and row > 0 and not data[row - 1][col] in ['#', 'O']:
             data[row][col] = '.'
 
-            self.roll(row - 1, col)
+            self.roll(row - 1, col, dir)
+
+        elif dir == 'West' and col > 0 and not data[row][col - 1] in ['#', 'O']:
+            data[row][col] = '.'
+
+            self.roll(row, col - 1, dir)
+
+        elif dir == 'South' and row + 1 < len(data) and not data[row + 1][col] in ['#', 'O']:
+            data[row][col] = '.'
+
+            self.roll(row + 1, col, dir)
+
+        elif dir == 'East' and col + 1 < len(data[0]) and not data[row][col + 1] in ['#', 'O']:
+            data[row][col] = '.'
+
+            self.roll(row, col + 1, dir)
 
     def calculate(self):
         data = self.data
