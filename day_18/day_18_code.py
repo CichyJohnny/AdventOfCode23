85a37d49891d3e53568ac73f4133df46b83297d6
Jan Cichy <04cichy@gmail.com>
2023-12-29 00:01:28 +0100

    speeded up version

--- day_18/day_18_code.py
+++ day_18/day_18_code.py
@@ -17,23 +17,22 @@ class Digging:
         for line in self.input:
             line = line.split()
 
-            direction = line[0]
             steps = int(line[1])
+            direction = line[0]
 
-            for i in range(steps):
-                data[loc] = line[2]
+            data[loc] = line[2]
 
-                if direction == 'R':
-                    loc = (loc[0], loc[1] + 1)
+            if direction == 'R':
+                loc = (loc[0], loc[1] + steps)
 
-                elif direction == 'L':
-                    loc = (loc[0], loc[1] - 1)
+            elif direction == 'L':
+                loc = (loc[0], loc[1] - steps)
 
-                elif direction == 'U':
-                    loc = (loc[0] - 1, loc[1])
+            elif direction == 'U':
+                loc = (loc[0] - steps, loc[1])
 
-                elif direction == 'D':
-                    loc = (loc[0] + 1, loc[1])
+            elif direction == 'D':
+                loc = (loc[0] + steps, loc[1])
 
     def count_enclosed_tiles(self):
         loop = list(self.data.keys())
@@ -41,11 +40,14 @@ class Digging:
         area = 0
         n = len(loop)
 
+        length = 0
         for i in range(n):
             j = (i + 1) % n  # Next tuple inside loop
 
+            length += abs(loop[i][0] - loop[j][0]) + abs(loop[i][1] - loop[j][1])
             area += loop[i][0] * loop[j][1]
             area -= loop[j][0] * loop[i][1]
 
         area = abs(area) // 2
-        self.answer = area + (n // 2 + 1)
+
+        self.answer = area + length // 2 + 1
