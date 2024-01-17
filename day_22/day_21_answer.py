125126fe2e8ca12070657fc668209a624ffc92c4
Jan Cichy <04cichy@gmail.com>
2024-01-17 22:04:11 +0100

    resolved day_21

--- day_22/day_21_answer.py
+++
@@ -1,8 +0,0 @@
-from day_21_code import Bricks
-
-# In day_21_input.txt put your unique input
-input = 'day_21_input.txt'
-
-answer = Bricks(input).start()
-
-print(answer)
---
+++ day_22/day_22_answer.py
@@ -0,0 +1,8 @@
+from day_22_code import Bricks
+
+# In day_22_input.txt put your unique input
+input = 'day_22_input.txt'
+
+answer = Bricks(input).start()
+
+print(answer)
